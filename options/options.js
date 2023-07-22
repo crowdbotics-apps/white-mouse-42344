089d7fc2c4aacfb650983d05b1eceb1a42a9d787
Crowdbotics <team@crowdbotics.com>
2023-07-22 06:27:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-mouse-42344",
+  url: "https://white-mouse-42344.botics.co",
+  api: "https://white-mouse-42344.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
